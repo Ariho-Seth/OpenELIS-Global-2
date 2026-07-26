535a83c2243bd2a9d75c733ce0277b38da185805
Atwijukire Ariho Seth <120330466+Ariho-Seth@users.noreply.github.com>
2026-07-26 10:07:30 +0100

    Format Checking
    Signed-off-by: Atwijukire Ariho Seth <120330466+Ariho-Seth@users.noreply.github.com>

--- src/main/java/org/openelisglobal/analysisqaevent/valueholder/AnalysisQaEvent.java
+++ src/main/java/org/openelisglobal/analysisqaevent/valueholder/AnalysisQaEvent.java
@@ -13,7 +13,15 @@
  */
 package org.openelisglobal.analysisqaevent.valueholder;
 
-import jakarta.persistence.*;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import java.sql.Date;
 import lombok.Getter;
 import lombok.Setter;
--- src/main/java/org/openelisglobal/analysisqaeventaction/valueholder/AnalysisQaEventAction.java
+++ src/main/java/org/openelisglobal/analysisqaeventaction/valueholder/AnalysisQaEventAction.java
@@ -13,7 +13,16 @@
  */
 package org.openelisglobal.analysisqaeventaction.valueholder;
 
-import jakarta.persistence.*;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import java.sql.Date;
 import lombok.Getter;
 import lombok.Setter;
