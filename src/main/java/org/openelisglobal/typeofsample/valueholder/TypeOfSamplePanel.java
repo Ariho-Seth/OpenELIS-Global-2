a6eb32633e1d74618ed94cbb9727fb4dd10ad663
Atwijukire Ariho Seth <120330466+Ariho-Seth@users.noreply.github.com>
2026-08-27 10:36:56 +0100

    Fixed mapping errors in migrated entities
    Signed-off-by: Atwijukire Ariho Seth <120330466+Ariho-Seth@users.noreply.github.com>

--- src/main/java/org/openelisglobal/typeofsample/valueholder/TypeOfSamplePanel.java
+++ src/main/java/org/openelisglobal/typeofsample/valueholder/TypeOfSamplePanel.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import java.sql.Timestamp;
 import lombok.Getter;
 import lombok.Setter;
